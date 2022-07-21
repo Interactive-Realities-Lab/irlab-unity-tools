9896adb524e131c161c8c6010da9b338f3609236
Jeronimo Grandi <JGGRANDI@uncg.edu>
2022-07-21 18:13:05 -0400

    Added the first tool. Timers to set and control timers and timing in our projects

---
+++ Runtime/Timers/TimerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace IRLab.Tools.Timer
+{
+    public class TimerBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float duration = 1f;
+        [SerializeField] private UnityEvent onTimerEnd = null;
+
+        private Timer timer;
+
+        private void Start()
+        {
+            // Create a new timer and initialise it
+            timer = new Timer(duration);
+
+            // Subscribe to the OnTimerEnd event to be able to handle that scenario
+            timer.OnTimerEnd += HandleTimerEnd;
+        }
+
+        private void HandleTimerEnd()
+        {
+            // Alert any listeners that the timer has ended
+            onTimerEnd.Invoke();
+
+            // Remove this component
+            Destroy(this);
+        }
+
+        private void Update() => timer.Tick(Time.deltaTime);
+    }
+}
