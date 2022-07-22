74a2a0237b7b2c05096746b68b401c39d5bc7cc9
Jeronimo Grandi <JGGRANDI@uncg.edu>
2022-07-22 17:34:27 -0400

    Modified the implementation of the timer

--- Runtime/Timers/Timer.cs
+++ Runtime/Timers/Timer.cs
@@ -1,37 +1,132 @@
-﻿using System;
+﻿/*
+ * Based on https://unitycodemonkey.com/utils.php
+ */
+
+using System;
+using UnityEngine;
+using System.Collections.Generic;
 
 namespace IRLab.Tools.Timer
 {
     public class Timer
     {
-        public float RemainingSeconds { get; private set; }
-
-        public Timer(float duration) => RemainingSeconds = duration;
-
-        public event Action OnTimerEnd;
-
-        public void Tick(float deltaTime)
+        private class MonoBehaviourHook : MonoBehaviour
         {
-            // Stop ticking if the timer has already ended
-            if (RemainingSeconds == 0f) { return; }
+            public Action OnUpdate;
 
-            // Tick the timer down by the time it took to complete last frame
-            RemainingSeconds -= deltaTime;
-
-            // Check to see if the timer has finished ticking
-            CheckForTimerEnd();
+            private void Update()
+            {
+                if (OnUpdate == null) return;
+                OnUpdate();
+            }
         }
 
-        private void CheckForTimerEnd()
+        private static List<Timer> timerList;
+        private static GameObject initGameObject;
+
+        private static void InitIfNeeded()
         {
-            // Leave if there is still time left to tick
-            if (RemainingSeconds > 0f) { return; }
+            if (initGameObject != null) return;
 
-            // Set to zero due to duration possibly going below zero with the deltaTime subtraction
-            RemainingSeconds = 0f;
+            initGameObject = new GameObject("Timer_Global");
+            timerList = new List<Timer>();
+        }
 
-            // Alert any listeners that the timer has ended
-            OnTimerEnd?.Invoke();
+
+        public static Timer Create(Action action, float time)
+        {
+            return Create(action, time, "", false, false);
+        }
+
+        public static Timer Create(Action action, float time, string functionName)
+        {
+            return Create(action, time, functionName, false, false);
+        }
+
+        public static Timer Create(Action action, float time, string functionName, bool useUnscaledDeltaTime)
+        {
+            return Create(action, time, functionName, useUnscaledDeltaTime, false);
+        }
+        public static Timer Create(Action action, float time, string functionName, bool useUnscaledDeltaTime, bool stopAllWithSameName)
+        {
+            InitIfNeeded();
+
+            if (stopAllWithSameName)
+                StopAllTimersWithName(functionName);
+
+            GameObject obj = new GameObject("Function Timer Obj " + functionName, typeof(MonoBehaviourHook));
+            Timer timer = new Timer(obj, action, time, functionName, useUnscaledDeltaTime);
+            obj.GetComponent<MonoBehaviourHook>().OnUpdate = timer.Update;
+            timerList.Add(timer);
+
+            return timer;
+        }
+
+        public static void RemoveTimer(Timer timer)
+        {
+            InitIfNeeded();
+            timerList.Remove(timer);
+        }
+
+        public static void StopAllTimersWithName(string functionName)
+        {
+            InitIfNeeded();
+
+            for (int i = 0; i < timerList.Count; i++)
+            {
+                if (timerList[i].functionName == functionName)
+                    timerList[i].DestroySelf();
+            }
+        }
+
+        public static void StopFirstTimerWithName(string functionName)
+        {
+            InitIfNeeded();
+            for (int i = 0; i < timerList.Count; i++)
+            {
+                if (timerList[i].functionName == functionName)
+                {
+                    timerList[i].DestroySelf();
+                    return;
+                }
+            }
+        }
+
+        private GameObject gameObject;
+        private float time;
+        private string functionName;
+        private bool active;
+        private bool useUnscaledDeltaTime;
+        private Action action;
+
+        public Timer(GameObject gameObject, Action action, float time, string functionName, bool useUnscaledDeltaTime)
+        {
+            this.gameObject = gameObject;
+            this.time = time;
+            this.functionName = functionName;
+            this.useUnscaledDeltaTime = useUnscaledDeltaTime;
+            this.action = action;
+        }
+
+        private void Update()
+        {
+            if (useUnscaledDeltaTime)
+                time -= Time.unscaledDeltaTime;
+            else
+                time -= Time.deltaTime;
+
+            if (time <= 0)
+            {
+                action();
+                DestroySelf();
+            }
+        }
+
+        private void DestroySelf()
+        {
+            RemoveTimer(this);
+            if (gameObject == null) return;
+            UnityEngine.Object.Destroy(gameObject);
         }
     }
 }
